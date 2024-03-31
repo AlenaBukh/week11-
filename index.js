31d46ae76c6c4cc1d1901e28b466f1d047879b20
AlenaBukh <alena89xxx@gmail.com>
2024-03-31 08:37:07 +0300

    add js function, style

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const totalPrice = document.querySelector(".total");
+const prices = document.querySelectorAll(".total");
 
-const getSale = () => {};
+const getSale = () => {
+  const totalPriceNum = Number(totalPrice.textContent);
+  totalPrice.textContent = totalPriceNum * 0.8;
+};
